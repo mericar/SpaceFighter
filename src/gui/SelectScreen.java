24f3a1fab18a1c3cf2e35eef19cd50d3ab6b2200
mirko <mirkocarich@gmail.com>
2015-11-14 23:40:22 -0800

    Added file for selectscreen

---
+++ src/gui/SelectScreen.java
@@ -0,0 +1,15 @@
+package gui;
+
+import spacefighter.Board;
+import spacefighter.Constants;
+
+import javax.swing.*;
+
+
+public class SelectScreen extends JFrame implements Constants {
+
+    public SelectScreen(){
+        //TODO: Implement selection home screen.
+    }
+
+}
