5812c1b764da22276b344971407f9f90ae3b587b
mirkmasterC <mirkocarich@gmail.com>
2015-12-16 02:53:52 -0800

    updated turret game screen

--- src/resources/Constants.java
+++ src/resources/Constants.java
@@ -3,7 +3,6 @@ package resources;
 /**
  * Created by M on 15-09-08.
  */
-// TODO: look into enum configuration
 
 public interface Constants {
     int BOARD_WIDTH = 1500;
@@ -13,7 +12,7 @@ public interface Constants {
     int ALIEN_HEIGHT = 12;
     int ALIEN_WIDTH = 12;
     int BORDER_RIGHT = 10;
-    int BORDER_LEFT =105;
+    int BORDER_LEFT = 105;
     int GO_DOWN = 50;
     int NUMBER_OF_ALIENS_TO_DESTROY = 5;
     int CHANCE = 5;
