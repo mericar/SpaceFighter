856ef2ee582d1539d31f2bf984416b8fa809ba2d
mirkmasterC <mirkocarich@gmail.com>
2016-01-06 00:12:10 -0800

    added mongodb jars, junit jars

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,24 @@
 package main;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.BulkWriteOperation;
+import com.mongodb.BulkWriteResult;
+import com.mongodb.Cursor;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.ParallelScanOptions;
+import com.mongodb.ServerAddress;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+
 import gui.SelectScreen;
 
 /**
@@ -8,7 +27,26 @@ import gui.SelectScreen;
 public class Main {
     // main
     public static void main(String[] args) {
+
+        MongoClient mongoClient = new MongoClient( "localhost", 8002 );
+        DB db = mongoClient.getDB( "mydb" );
+        System.out.println(db.getName());
+
         new SelectScreen();
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/AgentTests.java
+++ src/test/AgentTests.java
@@ -1,7 +1,12 @@
 package test;
 
+
+import org.junit.Test;
+
 /**
  * Created by mec on 2015-12-07.
  */
 public class AgentTests {
+
+
 }
